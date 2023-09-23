bbc5b2ab9d1257932083e8d646fda33c384f9269
vinay <domalavinaykumar20@gmail.com>
2023-09-23 11:49:27 +0530

    demo commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app=exp();
 const jwt=require("jsonwebtoken");
 const expressAsyncHandler=require("express-async-handler");
 const cors=require("cors");
+
+
 app.use(cors(
     {
         origin:'*'
@@ -13,6 +15,8 @@ require("dotenv").config();
 let DBurl=process.env.DBURL;
 let port=process.env.PORT || 4000;
 let mclient=require("mongodb").MongoClient;
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
 mclient.connect(DBurl)
 .then((client)=>{
     let dbobj=client.db("vnr");
@@ -25,6 +29,11 @@ mclient.connect(DBurl)
 .catch((error)=>{
     console.log("Error in Db connection",error);
 })
+
+app.get("/",(req,res)=>{
+res.send("Hello");
+})
+
 app.get("/get-user",expressAsyncHandler(async(request,response)=>{
     let userCollectionObj=app.get("userCollectionObj");
     let users=await userCollectionObj.find().toArray();
@@ -61,6 +70,57 @@ app.get("/get-booking",expressAsyncHandler(async(request,response)=>{
     response.send({message:"All bookings",payload:bookings});
 }))
 
+// app.delete("/del-booking/:id",expressAsyncHandler(async(request,response)=>{
+//     let id=(request.params.id);
+//     console.log(id);
+//     let bookingObj=app.get("bookingObj");
+//     await bookingObj.deleteOne({ _id: id });
+//     response.send({message:"ashkj"});
+// }))
+
+// app.delete("/del-booking/:id", expressAsyncHandler(async (request, response) => {
+//     const id = request.params.id;
+//     const bookingModel = app.get("bookingModel"); // Assuming you have a mongoose model named 'bookingModel' for bookings
+
+//     try {
+//         const deletedBooking = await bookingModel.findByIdAndDelete(id);
+//         if (deletedBooking) {
+//             response.send({ message: "Booking deleted successfully." });
+//         } else {
+//             response.status(404).send({ message: "Booking not found." });
+//         }
+//     } catch (error) {
+//         response.status(500).send({ message: "An error occurred while deleting the booking." });
+//     }
+// }));
+
+
+
+const mongoose = require("mongoose");
+
+app.delete("/del-booking/:id", expressAsyncHandler(async (request, response) => {
+    const id = request.params.id;
+    const convertedId = new mongoose.Types.ObjectId(id);
+    console.log(id);
+    let bookingObj=app.get("bookingObj");
+    try {
+        // const deletedBooking = await bookingObj.findByIdAndDelete(id);
+        const result = await bookingObj.deleteOne({ _id: convertedId });
+        console.log(result)
+        if (result.deleteCount!=0) {
+            response.send({ message: "Booking deleted successfully." });
+        } else {
+            response.status(404).send({ message: "Booking not found." });
+        }
+    } catch (error) {
+        console.error("Error deleting booking:", error);
+        response.status(500).send({ message: "An error occurred while deleting the booking." });
+    }
+}));
+
+
+
+
 app.use((request,response,next)=>{
     response.send({message:`path ${requsest.url} is invalid`})
 })
@@ -71,3 +131,6 @@ app.use((error,request,response,next)=>{
 app.listen(port,()=>{
     console.log("Server listening on port "+port);
 })
+
+
+module.exports=app;
